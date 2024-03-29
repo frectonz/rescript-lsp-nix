182de9ba62f85c1f1b449a8511e48ca297c970ba
frectonz <fraol0912@gmail.com>
2024-03-29 19:45:21 +0300

    include the analysis ocaml package

--- flake.nix
+++ flake.nix
@@ -13,9 +13,26 @@
       (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      with pkgs; {
-        packages.default = pkgs.buildNpmPackage rec {
+
+        rescript-analysis = pkgs.ocamlPackages.buildDunePackage rec {
+          pname = "analysis";
+          version = "1.50.0";
+
+          minimalOCamlVersion = "4.04";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "rescript-lang";
+            repo = "rescript-vscode";
+            rev = version;
+            hash = "sha256-4b2Z94/CCvPge9qKmv8svUib8zJ9NEZ+FYeylgmkKBQ=";
+          };
+
+          nativeBuildInputs = with pkgs; [ ocamlPackages.cppo ];
+        };
+
+        platformDir = if pkgs.stdenv.isLinux then "linux" else "darwin";
+
+        rescript-language-server = pkgs.buildNpmPackage rec {
           pname = "rescript-language-server";
           version = "1.50.0";
 
@@ -31,9 +48,15 @@
 
           nativeBuildInputs = [ pkgs.esbuild ];
           buildPhase = ''
+            npm install
+            mkdir analysis_binaries/linux
+            cp ${rescript-analysis}/bin/rescript-editor-analysis analysis_binaries/${platformDir}/rescript-editor-analysis.exe
             esbuild src/cli.ts --bundle --sourcemap --outfile=out/cli.js --format=cjs --platform=node --loader:.node=file --minify
           '';
         };
+      in
+      with pkgs; {
+        packages.default = rescript-language-server;
 
         formatter = nixpkgs-fmt;
       });
